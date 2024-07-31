8451eddfb8cb265e621f525f2807d37fc98f6621
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-07-31 14:47:48 +0000

    Adjust allowed host settings

--- relatorio_mercado/relatorio_mercado/settings.py
+++ relatorio_mercado/relatorio_mercado/settings.py
@@ -32,7 +32,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-atchutchi-relatoriomerc-erohn8wleru.ws-eu115.gitpod.io',
-    'https://observatorio-mercado-gw-ccc5b800c903.herokuapp.com/',
+    'observatorio-mercado-gw-ccc5b800c903.herokuapp.com',
     'localhost',
     ]
 
