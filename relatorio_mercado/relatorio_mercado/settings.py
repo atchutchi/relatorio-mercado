5d31216b5531416b4fa67f9aec83facc01f5cf74
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2024-08-01 08:36:56 +0000

    Added bucket config and static and media files to settigns

--- relatorio_mercado/relatorio_mercado/settings.py
+++ relatorio_mercado/relatorio_mercado/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-atchutchi-relatoriomerc-erohn8wleru.ws-eu115.gitpod.io',
     'https://observatorio-mercado-gw-7e203c4b172d.herokuapp.com/',
-    'http://localhost',  # ou 'https://localhost' se você estiver usando HTTPS localmente
+    'http://localhost',
 ]
 
 # Application definition
@@ -58,6 +58,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'relatorio_mercado.home',
     'relatorio_mercado.indicadores',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -179,6 +181,31 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'relatorio-mercado'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
